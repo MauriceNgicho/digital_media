236659d1bfe62356b9a5463fd22aa0afe11feea1
Maurice Ngicho <mrcngicho@gmail.com>
2025-10-08 21:19:42 +0300

    modified pages

--- app/page.js
+++ app/page.js
@@ -21,11 +21,3 @@ export default function Home() {
     </main>
   );
 }
-
-// export default function Home() {
-//   return (
-//     <h1 className="text-3xl font-bold underline">
-//       Hello world!
-//     </h1>
-//   )
-// }
